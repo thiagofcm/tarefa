bdb3db1d2fd2e767ad66257c08f98a407de8ae18
thiagofcm <thiagofcm1503@gmail.com>
2017-05-30 12:36:06 -0300

    parte 1 concluida

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject menuPanel;
+    public GameObject cutScene;
+
+    public static GameController instancia = null;
+
+    private void Awake() {
+        if (instancia == null) {
+            instancia = this;
+        }
+        else if (instancia != null) {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    public void TrPlayCutScene() {
+        menuPanel.SetActive(false);
+        Instantiate(cutScene);
+    }
+
+    public void TriFinishCutScene() {
+        menuPanel.SetActive(true);
+    }
+}
